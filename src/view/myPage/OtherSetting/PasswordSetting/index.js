dc04159706ba1b93e59eb5deaff617637413a2e0
2395527697 <2395527697@qq.com>
2021-07-07 15:57:39 +0800

    1.新增国际化字段。2.修改样式。3.新增密码设置页面，完善校验逻辑

--- src/view/myPage/OtherSetting/PasswordSetting/index.js
+++ src/view/myPage/OtherSetting/PasswordSetting/index.js
@@ -1,10 +1,74 @@
+import { Input, Button, Form } from 'antd'
 import { useTranslation } from 'react-i18next'
 const PasswordSetting = () => {
   const { t } = useTranslation()
+  const handleFinish = (values) => {
+    console.log(values);
+  }
   return (
     <div>
       <div className='myPageTitle' id='PasswordSetting'>{t(73)}</div>
-    </div>
+      <div className='labelInfo'>{t(107)}</div>
+      <div className='PasswordSettingBox'>
+        <Form
+          name="basic"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 18 }}
+          onFinish={handleFinish}
+        >
+          <Form.Item
+            label={t(108)}
+            name="oldPassword"
+            rules={[
+              ({ getFieldValue }) => ({
+                validator (_, value) {
+                  if (/^\d{6,20}$/.test(value)) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error(t(41)));
+                }
+              })
+            ]}
+          >
+            <Input.Password placeholder={t(41)} allowClear />
+          </Form.Item>
+          <Form.Item
+            label={t(109)}
+            name="newPassword"
+            dependencies={['oldPassword']}
+            rules={[
+              ({ getFieldValue }) => ({
+                validator (_, value) {
+                  if (!value || getFieldValue('oldPassword') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error(t(110)));
+                }
+              })
+            ]}
+          >
+            <Input.Password placeholder={t(41)} allowClear />
+          </Form.Item>
+          <Form.Item
+            label={t(37)}
+            name="confirmPassword"
+            rules={[({ getFieldValue }) => ({
+              validator (_, value) {
+                if (/^\d{6,20}$/.test(value)) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error(t(41)));
+              }
+            })]}
+          >
+            <Input.Password placeholder={t(41)} allowClear />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 11 }}>
+            <Button type="primary" htmlType="submit">{t(19)}</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div >
   )
 }
 export default PasswordSetting
